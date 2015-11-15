b2b8cfd0b293e0a7c5e10ea19129ef37f2fc2d26
Bobby Smith <bobbysmith24@gmail.com>
2015-11-14 19:51:01 -0600

    initial

---
+++ app.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var request = require('request');
+var catbot = require('./catbot');
+
+var app = express();
+
+var port = process.env.PORT || 3000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/', function (req, res) {
+  res.status(200).send('Hello world!');
+});
+
+app.post('/cat', catbot);
+
+// error handler
+app.use(function (err, req, res, next) {
+  console.error(err.stack);
+  res.status(400).send(err.message);
+});
+
+app.listen(port, function () {
+  console.log('Slack bot listening on port ' + port);
+});
---
+++ catbot.js
@@ -0,0 +1,26 @@
+var request = require('request');
+var requestify = require('requestify');
+
+module.exports = function (req, res, next) {
+
+  var catUrl;
+
+  requestify.get('http://random.cat/meow').then(function (response) {
+    response.getBody();
+
+    var resBody = JSON.parse(response.body);
+    catUrl = resBody.file;
+    console.log(catUrl);
+
+    var username = req.body.user_name;
+    var botPayload = { text: 'hello @' + username + ' ' + catUrl};
+    console.log(botPayload);
+
+    if (username !== 'slackbot') {
+      return res.status(200).json(botPayload);
+    } else {
+      return res.status(200).end();
+    }
+  });
+
+}
